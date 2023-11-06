d0d33782dbc3a776490950fb43e7f18ba87ccb9b
sthapa <sureshthapa409@gmail.com>
2023-11-06 16:18:49 +0545

    forget password mail done

--- internal/service/service.go
+++ internal/service/service.go
@@ -3,11 +3,13 @@ package service
 import (
 	"example.com/dynamicWordpressBuilding/internal/repository"
 	"example.com/dynamicWordpressBuilding/utils"
+	"example.com/dynamicWordpressBuilding/utils/mailer"
 )
 
 type Service struct {
 	repo       repository.RepoInterface
 	tokenMaker utils.Maker
+	mailJet    mailer.MailService
 }
 
 type ServiceInterface interface {
@@ -19,5 +21,6 @@ func NewService(repo repository.RepoInterface) ServiceInterface {
 	svc.repo = repo
 	svc.repo = repository.NewRepo()
 	svc.tokenMaker = utils.NewTokenMaker()
+	svc.mailJet = mailer.NewMailService()
 	return svc
 }
